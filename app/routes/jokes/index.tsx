360ca4e9ec7d91fb137597ffb85b30fa90d78335
wkylin.w <wkylin.w@gmail.com>
2023-02-19 22:11:28 +0800

    first commit

---
+++ app/routes/jokes/index.tsx
@@ -0,0 +1,28 @@
+import { json } from "@remix-run/node";
+import { useLoaderData, Link } from "@remix-run/react";
+
+import { db } from "~/utils/db.server";
+
+export const loader = async () => {
+  const count = await db.joke.count();
+  const randomRowNumber = Math.floor(Math.random() * count);
+  const [randomJoke] = await db.joke.findMany({
+    take: 1,
+    skip: randomRowNumber,
+  });
+  return json({ randomJoke });
+};
+
+export default function JokesIndexRoute() {
+  const data = useLoaderData<typeof loader>();
+
+  return (
+    <div>
+      <p>Here's a random joke:</p>
+      <p>{data.randomJoke.content}</p>
+      <Link to={data.randomJoke.id}>
+        "{data.randomJoke.name}" Permalink
+      </Link>
+    </div>
+  );
+}
